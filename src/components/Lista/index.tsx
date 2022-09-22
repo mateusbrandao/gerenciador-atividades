b12df94827db96053a679caff6078d1ae457f6d7
Mateus Brandao Ramos Carneiro <mateusbrandao@id.uff.br>
2022-09-21 21:43:46 -0300

    Item passa a pertencer a um script proprio e passaa user props

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import Item from './Item'
 import style from './Lista.module.scss';
 
 function Lista() {
     const tarefas = [{
         tarefa: 'React',
         tempo: '02:00:00'
-    },{
+    }, {
         tarefa: 'JavaScript',
         tempo: '01:00:00'
-    },{
+    }, {
         tarefa: 'TypeScript',
         tempo: '03:00:00'
     }
@@ -18,11 +19,11 @@ function Lista() {
         <aside className={style.listaTarefas}>
             <h2>Estudos do dia</h2>
             <ul>
-                {tarefas.map((item,index)=>(
-                    <li key={index} className={style.item}>
-                        <h3>{item.tarefa}</h3>
-                        <span>{item.tempo}</span>
-                    </li>
+                {tarefas.map((item, index) => (
+                    <Item
+                        key={index}
+                        {...item}
+                    />
                 ))}
             </ul>
         </aside>
