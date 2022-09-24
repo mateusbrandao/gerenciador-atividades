2100f312b36ca8473923cc3e5a548044670a22a3
Mateus Brandao Ramos Carneiro <mateusbrandao@id.uff.br>
2022-09-23 21:18:39 -0300

    Criada logica para selecionar tarefas. Alterado css do item para uma pasta propria. Criada conversao de tempo para segundos

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,11 +1,24 @@
+import { useState } from 'react';
+
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from './Cronometro.module.scss'
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
 
-export default function Cronometro() {
+interface Props{
+    selecionado: ITarefa | undefined
+}
+
+export default function Cronometro({selecionado}: Props) {
+    const [tempo, setTempo] = useState<number>();
+    if(selecionado?.tempo){
+        setTempo(tempoParaSegundos(selecionado?.tempo))
+    }
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronometro</p>
+            Tempo: {tempo}
             <div className={style.relogioWrapper}>
                 <Relogio/>
             </div>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -7,11 +7,24 @@ import style from './App.module.scss';
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const[selecinado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa){
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false 
+    })));
+  }
+
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas}/>
-      <Cronometro/>
-      <Lista tarefas={tarefas}/>
+      <Cronometro selecionado={selecinado}/>
+      <Lista 
+        tarefas={tarefas}
+        selecionaTarefa={selecionaTarefa}
+      />
     </div>
   );
 }
