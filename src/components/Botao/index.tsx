dae5cc1e8b0b3e1d2bc6673b7cab4ff9049bb8f4
Mateus Brandao Ramos Carneiro <mateusbrandao@id.uff.br>
2022-09-24 19:51:04 -0300

    Refatorados componentes Botao e Formulario para que passem a user functioncomponents

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,16 +1,16 @@
 import React,  { ReactFragment } from "react";
 import style from './Botao.module.scss';
 
-type Params = {
-    type?: "button" | "submit" | "reset" | undefined;
-    children?: ReactFragment | undefined;
-    onClick?: () => void;
+interface Props {
+    type?: "button" | "submit" | "reset" | undefined,
+    onClick?: () => void,
+    children?: React.ReactNode
 }
 
-const Botao: React.FC<Params> = (props:Params) =>{   
+function Botao({onClick, type, children}: Props){   
     return(
-        <button onClick={props.onClick} type={props.type} className={style.botao}>
-            {props.children}
+        <button onClick={onClick} type={type} className={style.botao}>
+            {children}
         </button>
     )
 }
